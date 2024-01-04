26d47d419b350e24de3edf7730536471d3567cdd
Lincoln Nogueira <lincolnthalles@users.noreply.github.com>
2024-01-04 02:50:36 -0300

    chore: update deno deps

--- deps.ts
+++ deps.ts
@@ -13,25 +13,26 @@ export {
     assertIsError,
     assertStringIncludes,
     assertThrows,
-} from "https://deno.land/std@0.208.0/assert/mod.ts#^";
+} from "https://deno.land/std@0.210.0/assert/mod.ts#^";
 
-export { crypto } from "https://deno.land/std@0.208.0/crypto/mod.ts#^";
-export { encodeHex } from "https://deno.land/std@0.208.0/encoding/hex.ts#^";
-export { existsSync } from "https://deno.land/std@0.208.0/fs/mod.ts#^";
-export { isAbsolute } from "https://deno.land/std@0.208.0/path/mod.ts#^";
+export { crypto } from "https://deno.land/std@0.210.0/crypto/mod.ts#^";
+export { encodeHex } from "https://deno.land/std@0.210.0/encoding/hex.ts#^";
+export { existsSync } from "https://deno.land/std@0.210.0/fs/mod.ts#^";
+export { isAbsolute } from "https://deno.land/std@0.210.0/path/mod.ts#^";
 
 import osPaths from "https://deno.land/x/os_paths@v7.4.0/src/mod.deno.ts#^";
 export { osPaths };
 
-// @deno-types="https://cdn.skypack.dev/fflate@0.8.1/lib/index.d.ts#^"
-// export * as fflate from "https://cdn.skypack.dev/fflate@0.8.0?min#^";
-
-export { Readable } from "node:stream";
-export { finished } from "node:stream/promises";
-export * as fs from "node:fs";
+/// @deno-types="https://cdn.skypack.dev/fflate@0.8.1/lib/index.d.ts#^"
+/// export * as fflate from "https://cdn.skypack.dev/fflate@0.8.1?min#^";
 
 // @deno-types="npm:@types/decompress@4.2.7"
 import decompress from "npm:decompress@4.2.1";
 export { decompress };
 
 export { defineConfig } from "npm:vite@^4.5.1";
+
+// @deno-types="npm:@types/node"
+export { Readable } from "node:stream";
+export { finished } from "node:stream/promises";
+export * as fs from "node:fs";
