cd4688eca211483b6f6799fe925f4106eb57aadc
Lincoln Nogueira <lincolnthalles@users.noreply.github.com>
2025-02-05 14:15:17 -0300

    chore: tweak build script

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,16 +1,24 @@
-/// Create dummy server binaries in case they don't exist.
+use std::{env, fs};
+
+#[cfg(unix)]
+use std::os::unix::fs::{MetadataExt, PermissionsExt};
+
+/// Ensure that the build dependencies exist in some form.
 ///
 /// This makes the linter happy when the repository is not fully set up.
-fn ensure_dummy_deps() {
-    let src_ui = std::env::current_dir()
-        .expect("Failed to get current directory!")
-        .join("../src-ui/build");
-    std::fs::create_dir_all(&src_ui).ok();
+///
+/// - Create UI build directory.
+/// - Create dummy server binaries in case they don't exist.
+///   > The Tauri build hook will download the proper binaries later.
+/// - Set the executable bit on valid UNIX server binaries.
+fn ensure_deps() {
+    let current_dir = env::current_dir().expect("Failed to get current directory!");
 
-    let server_dist = std::env::current_dir()
-        .expect("Failed to get current directory!")
-        .join("../server-dist");
-    std::fs::create_dir_all(&server_dist).ok();
+    let src_ui = current_dir.join("../src-ui/build");
+    fs::create_dir_all(&src_ui).ok();
+
+    let server_dist = current_dir.join("../server-dist");
+    fs::create_dir_all(&server_dist).ok();
 
     let dist_path = server_dist
         .canonicalize()
@@ -25,12 +33,22 @@ fn ensure_dummy_deps() {
     for target in TARGETS {
         let target_bin = dist_path.join(format!("memos-{}", target));
         if !target_bin.exists() {
-            std::fs::File::create(&target_bin).ok();
+            std::fs::File::create(&target_bin).expect("Failed to create dummy binary!");
+        }
+        #[cfg(unix)]
+        {
+            let meta = fs::metadata(&target_bin).expect("Failed to get file metadata!");
+            if meta.size() < 1024 {
+                continue;
+            }
+            let mut perms = meta.permissions();
+            perms.set_mode(0o755);
+            fs::set_permissions(&target_bin, perms).expect("Failed to set file permissions!");
         }
     }
 }
 
 fn main() {
-    ensure_dummy_deps();
+    ensure_deps();
     tauri_build::build()
 }
