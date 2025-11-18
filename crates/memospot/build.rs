ac2792c9a0b6dfd11cd7a8db600f4c3a4c247a7c
Lincoln Nogueira <lincolnthalles@users.noreply.github.com>
2025-11-18 17:55:18 -0300

    chore: improve build script

--- crates/memospot/build.rs
+++ crates/memospot/build.rs
@@ -3,13 +3,40 @@ use std::{env, fs, path::PathBuf};
 #[cfg(unix)]
 use std::os::unix::fs::{MetadataExt, PermissionsExt};
 
-/// Detect the workspace root.
-fn detect_workspace_root() -> Option<PathBuf> {
+fn find_workspace_root() -> Option<PathBuf> {
     const MAX_DEPTH: usize = 5;
     let mut current_dir = env::current_dir().ok()?;
 
     for _ in 0..=MAX_DEPTH {
         if current_dir.join(".git").exists() && current_dir.join(".gitattributes").exists() {
+            return Some(current_dir);
+        }
+        if !current_dir.pop() {
+            return None;
+        }
+    }
+    None
+}
+
+/// Find the target directory.
+///
+/// Workaround for <https://github.com/rust-lang/cargo/issues/5457>.
+///
+/// See <https://github.com/tauri-apps/tauri/blob/22edc65a/crates/tauri-build/src/lib.rs#L522>.
+fn find_target_dir() -> Option<PathBuf> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap_or_default());
+    if out_dir == PathBuf::default() {
+        return None;
+    }
+
+    const MAX_DEPTH: usize = 5;
+    let mut current_dir = env::current_dir().ok()?;
+
+    for _ in 0..=MAX_DEPTH {
+        if current_dir.join(".cargo-lock").exists() {
+            return Some(current_dir);
+        }
+        if current_dir.join(".fingerprint").exists() && current_dir.join("build").exists() {
             return Some(current_dir);
         }
         if !current_dir.pop() {
@@ -28,7 +55,7 @@ fn detect_workspace_root() -> Option<PathBuf> {
 ///   > The Tauri build hook will download the proper binaries later.
 /// - Set the executable bit on valid UNIX server binaries.
 fn ensure_deps() {
-    let workspace_root = detect_workspace_root().expect("Failed to detect workspace root!");
+    let workspace_root = find_workspace_root().expect("Failed to detect workspace root!");
 
     let src_ui = workspace_root.join("src-ui/build");
     fs::create_dir_all(&src_ui).ok();
@@ -48,9 +75,18 @@ fn ensure_deps() {
     ];
     for target in TARGETS {
         let target_bin = dist_path.join(format!("memos-{target}"));
+
         if !target_bin.exists() {
-            std::fs::File::create(&target_bin).expect("Failed to create dummy binary!");
+            #[cfg(feature = "unittest")]
+            {
+                std::fs::File::create(&target_bin).expect("Failed to create dummy binary!");
+            }
+            #[cfg(not(feature = "unittest"))]
+            {
+                continue;
+            }
         }
+
         #[cfg(unix)]
         {
             let meta = fs::metadata(&target_bin).expect("Failed to get file metadata!");
@@ -64,7 +100,31 @@ fn ensure_deps() {
     }
 }
 
+/// Cleanup dummy server binaries.
+///
+/// Prevents dev mode to start with a dummy server binary.
+fn cleanup_dummy_deps() {
+    let Some(target_dir) = find_target_dir() else {
+        return;
+    };
+
+    let is_windows = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows";
+    let memos_bin = target_dir.join(if is_windows { "memos.exe" } else { "memos" });
+    if !memos_bin.exists() {
+        return;
+    }
+    if let Ok(meta) = memos_bin.metadata() {
+        if meta.len() == 0 {
+            fs::remove_file(memos_bin).ok();
+        }
+    }
+}
+
 fn main() {
     ensure_deps();
+    // Runs only on dev and release builds.
+    if cfg!(not(feature = "unittest")) {
+        cleanup_dummy_deps();
+    }
     tauri_build::build()
 }
